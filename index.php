dfb4880d0ee3bbcf672e5761c16c921b0cc71b4a
luca-masera <lucamasera1995@gmail.com>
2023-11-28 15:32:27 +0100

    initial commit + array

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+$games = [
+    [
+        "teamHome" => "Lakers",
+        "teamOut" => "Clippers",
+        "homeScore" => 89,
+        "outScore" => 87,
+    ],
+    [
+        "teamHome" => "Suns",
+        "teamOut" => "Denver",
+        "homeScore" => 90,
+        "outScore" => 110,
+    ],
+    [
+        "teamHome" => "Boston",
+        "teamOut" => "Pistons",
+        "homeScore" => 99,
+        "outScore" => 88,
+    ],
+    [
+        "teamHome" => "Bulls",
+        "teamOut" => "Rockets",
+        "homeScore" => 118,
+        "outScore" => 95,
+    ]
+]
+    ?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>ciao</h1>
+</body>
+
+</html>
